6c90e752d0bb2ab628c2ea9620e5a8767c030d75
EndriGuma <andrewgouma@gmail.com>
2024-12-23 00:06:03 +0100

    hello world

--- blogs.go
+++ blogs.go
@@ -6,10 +6,12 @@ type BlogPost struct {
 	Content         string
 	Slug            string
 	MetaDescription string
+	Date	string
+	ImgSource string
 }
 
 // BlogPosts is a slice containing all blog posts
 var BlogPosts = []BlogPost{
-	{Title: "First Post", Content: "This is the first blog post.", Slug: "first-post", MetaDescription: "This is a brief summary of the first blog post."},
-	{Title: "Second Post", Content: "This is the second blog post.", Slug: "second-post", MetaDescription: "This is a brief summary of the second blog post."},
+	{Title: "First Post", Content: "This is the first blog post.", Slug: "first-post", MetaDescription: "This is a brief summary of the first blog post.", Date: "20 December 2024", ImgSource: "assets/images/home/lawyer.png"},
+	{Title: "Second Post", Content: "This is the second blog post.", Slug: "second-post", MetaDescription: "This is a brief summary of the second blog post.", Date: "20 December 2024", ImgSource: "assets/images/home/lawyer.png"},
 }
